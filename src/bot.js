25f89d01c0d6681e7a724a7e5c855f8a41ad6599
Landon Gravat <railinator4903@gmail.com>
2019-01-06 21:57:24 -0600

    oof import

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Discord = require('discord.js');
+const Octokit = require('@octokit/rest');
 
 class Bot {
     constructor(configPath) {
